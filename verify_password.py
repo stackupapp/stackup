4afdeb7ca0c6f6be19a5eacf61f2511d3ad148d3
stackupapp <stackupapp@gmail.com>
2025-05-08 10:39:21 -0400

    Delete verify_password.py

--- verify_password.py
+++
@@ -1,7 +0,0 @@
-import streamlit_authenticator as stauth
-
-hashed = '$2b$12$qXnrU6jNBfjW6VUJYKinNOQkw.nChAqhgkwttmx7kN0OgRQUtk49S'
-password = 'stack123'
-
-hasher = stauth.Hasher()
-print(hasher.verify(password, hashed))  # Should print True
